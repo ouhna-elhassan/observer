a10eb2f425cba2c863a3fffe9ad029a5877a22eb
ouhna-elhassan <ouhnahassan01@gmail.com>
2022-10-17 22:54:48 +0100

    first commit

---
+++ Client.java
@@ -0,0 +1,19 @@
+//La classe Client.java
+
+import java.util.Observer;
+
+public class Client {
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        Subject subject = new Subject(0);
+        Observer binary = new BinaryObserver(subject);
+        Observer octal = new OctalObserver(subject);
+        Observer hexa = new HexaObserver(subject);
+        Thread thread = new Thread(subject);
+        thread.start();
+    }
+
+}
---
+++ Subject.java
@@ -0,0 +1,55 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Observable;
+import java.util.Observer;
+
+public class Subject extends Observable implements Runnable  {
+
+    //private List<Observer> observers = new ArrayList<Observer>();
+    private int state;
+
+    public Subject(int val) {
+        this.setState(val);
+    }
+
+    public int getState() {
+        return state;
+    }
+
+    public void setState(int state) {
+        this.state = state;
+        //notifyAllObservers();
+        setChanged();
+        notifyObservers(this.getState());
+    }
+
+    public void subscribe(Observer observer){
+        this.addObserver(observer);
+        //observers.add(observer);
+    }
+
+    public void unsubscribe(Observer observer){
+        this.deleteObserver(observer);
+        //observers.remove(observer);
+    }
+
+    /*public void notifyAllObservers(){
+        for (Observer observer : observers) {
+            observer.update();
+        }
+    }*/
+
+
+    @Override
+    public void run() {
+        try {
+            while (!Thread.currentThread().isInterrupted()) {
+                this.setState((this.getState() + 1) % 60);
+                Thread.sleep(2000);
+            }
+        } catch (InterruptedException e) {
+        }
+    }
+
+
+}
